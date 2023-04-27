307d70ef43a07ad24e6c2b495c8186ae27921d16
Preston Harberts <41871405+prestonharberts@users.noreply.github.com>
2023-04-27 10:53:57 -0500

    add performance commands to bin

--- bin/gaming-mode.sh
+++ bin/gaming-mode.sh
@@ -2,5 +2,10 @@
 
 proc=`pidof tresorit tresorit-daemon syncthing`
 kill $proc
+
 sudo cp ~/src/fedora-37-configuration/conf/keyd/gaming/default.conf /etc/keyd/
 sudo keyd reload
+
+gsettings set org.gnome.shell disable-user-extensions true
+
+echo performance | sudo tee /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor
--- bin/productivity-mode.sh
+++ bin/productivity-mode.sh
@@ -3,5 +3,9 @@
 ~/.local/share/tresorit/tresorit-cli start
 systemctl --user daemon-reload
 systemctl --user restart syncthing.service
+
 sudo cp ~/src/fedora-37-configuration/conf/keyd/productivity/default.conf /etc/keyd/
 sudo keyd reload
+
+gsettings set org.gnome.shell disable-user-extensions false
+echo powersave | sudo tee /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor
