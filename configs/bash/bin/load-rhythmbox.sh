5f56f3beb7f3fdd31970dbf9380cffd23ab7d427
Preston Harberts <41871405+prestonharberts@users.noreply.github.com>
2023-10-02 16:13:06 -0500

    add scripts to sync Rhythmbox

---
+++ configs/bash/bin/load-rhythmbox.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+cp ~/mus/rhythmbox/playlists.xml ~/mus/rhythmbox/rhythmdb.xml ~/.local/share/rhythmbox/
---
+++ configs/bash/bin/save-rhythmbox.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+mv ~/.local/share/rhythmbox/playlists.xml ~/.local/share/rhythmbox/rhythmdb.xml ~/mus/rhythmbox/
