059a9659eff9bf855524dc9ad35eceaad96211b1
Preston Harberts <41871405+prestonharberts@users.noreply.github.com>
2024-02-23 16:38:47 -0600

    install Decibels

---
+++ docs/decibels.sh
@@ -0,0 +1,9 @@
+# Decibels
+
+Run the following to install Decibels:
+
+```
+flatpak install flathub com.vixalien.decibels -y
+```
+
+Set Decibels as the default app for any audio files you want brought up for quick listening without having to import them into Rhythmbox.
